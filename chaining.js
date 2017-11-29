08c9b86f2a42eb1321288584d4c85cb2acdc105a
Tim A. Conner <timaconner1@gmail.com>
2017-11-29 11:31:39 -0600

    Add multiply each remaining number by 1.5 and then subtract 1.

--- chaining.js
+++ chaining.js
@@ -1,5 +1,10 @@
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
 // document.getElementById("output").innerHTML = 
-integers.sort(function(a, b){return b-a}).splice(0, 4).;
+integers = integers.sort(function(a, b){return b-a}).slice(integers.findIndex(x => x < 19), integers.length).map(y => ((y*1.5)-1));
+
 console.log(integers);
 
+/*
+.slice(0, 
+
+*/
