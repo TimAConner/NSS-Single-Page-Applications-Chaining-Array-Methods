9c348a187bec5bdea00046b430b0d857bcab85ba
Tim A. Conner <timaconner1@gmail.com>
2017-11-29 11:05:59 -0600

    Add remove any integers greater than 19.

--- chaining.js
+++ chaining.js
@@ -1,4 +1,5 @@
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
-integers.sort(function(a, b){return b-a});
-
+// document.getElementById("output").innerHTML = 
+integers.sort(function(a, b){return b-a}).splice(0, 4).;
 console.log(integers);
+
