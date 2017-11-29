53f45c261da6633a8d6e41eb3cef04c0af4d5852
Tim A. Conner <timaconner1@gmail.com>
2017-11-29 11:41:10 -0600

    Changed sort to use arrow function.

--- chaining.js
+++ chaining.js
@@ -1,3 +1,3 @@
 var integers = [13, 25, 6, 3, 11, 2, 18, 7, 21, 1, 29, 20, 12, 8];
-document.getElementById("output").innerHTML = integers.sort(function(a, b){return b-a}).slice(integers.findIndex(x => x < 19), integers.length).map(y => ((y*1.5)-1)).reduce((a, b) => a + b, 0);
+document.getElementById("output").innerHTML = integers.sort((a, b) => b-a).slice(integers.findIndex(x => x < 19), integers.length).map(y => ((y*1.5)-1)).reduce((a, b) => a + b, 0);
 
